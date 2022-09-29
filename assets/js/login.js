bef04f85f5c60aa7f7f1d262907b0c353c1ba679
lihao1011 <lh17337669012@163.com>
2022-09-29 14:30:53 +0800

    完成了注册页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,50 @@ $(function(){
     $('.reg-wrap').hide()
     $('.login-wrap').show()
   })
+  const form = layui.form
+  const layer = layui.layer
+  form.verify({
+    pwd: [
+    /^[\S]{6,12}$/
+    ,'密码必须6到12位，且不能出现空格'
+  ] 
+,
+  repwd: function(value){
+    if($('#password').val()!==value){
+  return '两次密码输入不一致，请重新输入'
+    }
+ } 
+})
+
+
+
+const format2Json = (source)=>{
+  let target = {}
+   source.split('&').forEach((el)=>{
+
+    let kv = el.split('=')
+    target[kv[0]] = kv[1]
+   })
+return JSON.stringify(target)
+
+}
+$('#formReg').on('submit',function(e){
+e.preventDefault()
+$.ajax({
+  method:'POST',
+  url:'http://big-event-vue-api-t.itheima.net/api/reg',
+  contentType:'application/json',
+  // data:JSON.stringify({
+  //   username:$('.reg-wrap [name=username]').val(),
+  //   password:$('.reg-wrap [name=password]').val(),
+  //   repassword:$('.reg-wrap [name=repassword]').val()
+  // }),
+  data:format2Json($(this).serialize()),
+  success(res){
+  if(res.code!==0)  return layer.msg(res.message)
+  layer.msg('注册成功');
+  $('#go2login').click()
+  }
+})
+})
 })
